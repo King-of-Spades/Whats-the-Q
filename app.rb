6a0a4f1494201992485944c749824ad566ad388e
aslonski <andrey@intercom.io>
2018-12-17 10:02:04 -0600

    live canvas testing

--- app.rb
+++ app.rb
@@ -4,29 +4,28 @@ require 'net/http'
 require 'intercom'
 require 'dotenv/load'
 
+# https://9a6d8a02.ngrok.io
 
 $intercom = Intercom::Client.new(token: ENV['TOKEN'])
 $all_convos = $intercom.counts.for_type(type: 'conversation').conversation["open"]
 p $all_convos
 
-# get '/' do
-# 	p "Hi! #{$all_convos}"
-# end
-
-# post '/' do
-# 	  content_type 'application/json'
-
-# 	text = "{\"canvas\":{\"content_url\":\"https://frozen-badlands-35106.herokuapp.com/live_canvas\"}}"
-#  	text.to_json
-# 	text
-# end
+get '/' do
+	p "Hi! #{$all_convos}"
+end
 
 post '/' do
-  # content_type 'application/json'
+	text = "{\"canvas\":{\"content\":{\"components\":[{\"id\":\"ab1c31592d25779a24e25b2e97b4\",\"type\":\"text\",\"text\":\"There are currently #{$all_convos} conversations in queue.\",\"style\":\"header\",\"align\":\"left\",\"bottom_margin\":false},{\"id\":\"81e44d7877d568cdb57883b1fa20\",\"type\":\"button\",\"label\":\"Refresh \",\"style\":\"primary\",\"action\":{\"type\":\"submit\",\"url\":null},\"bottom_margin\":false}]},\"stored_data\":{}}}"
+ 	text.to_json
+	text
+end
+
+post '/live_canvas' do
+  content_type 'application/json'
 		
 $all_convos = $intercom.counts.for_type(type: 'conversation').conversation["open"]
 
-	text = "{\"canvas\":{\"content\":{\"components\":[{\"id\":\"ab1c31592d25779a24e25b2e97b4\",\"type\":\"text\",\"text\":\"There are currently #{$all_convos} conversations in queue.\",\"style\":\"header\",\"align\":\"left\",\"bottom_margin\":false},}]}}}"
+	text = "{\"canvas\":{\"content\":{\"components\":[{\"id\":\"ab1c31592d25779a24e25b2e97b4\",\"type\":\"text\",\"text\":\"There are currently #{$all_convos} conversations in queue.\",\"style\":\"header\",\"align\":\"left\",\"bottom_margin\":false},}]},\"stored_data\":{}}}"
  	text.to_json
 	text
 end
