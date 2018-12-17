f7fa73aab757db439b0280f01a36e0b507efcf3e
aslonski <andrey@intercom.io>
2018-12-17 10:16:10 -0600

    live canvas testing

--- app.rb
+++ app.rb
@@ -23,7 +23,7 @@ end
 post '/live_canvas' do
   content_type 'application/json'
 		
-$all_convos = $intercom.counts.for_type(type: 'conversation').conversation["open"]
+  $all_convos = $intercom.counts.for_type(type: 'conversation').conversation["open"]
 
 
 	text = "{\"content\":{\"components\":[{\"id\":\"ab1c31592d25779a24e25b2e97b4\",\"type\":\"text\",\"text\":\"There are currently #{$all_convos} conversations in queue.\",\"style\":\"header\",\"align\":\"left\",\"bottom_margin\":false},{\"id\":\"81e44d7877d568cdb57883b1fa20\",\"type\":\"button\",\"label\":\"Refresh \",\"style\":\"primary\",\"action\":{\"type\":\"submit\",\"url\":null},\"bottom_margin\":false}]}}"
