30593b7c3d126ff1a7cc163272635a523d7e939d
aslonski <andrey@intercom.io>
2018-12-17 10:48:32 -0600

    live canvas testing

--- app.rb
+++ app.rb
@@ -30,14 +30,15 @@ post '/live_canvas' do
  	text.to_json
 	text
 
-	post '/submit' do
-		content_type 'application/json'
+	# post '/submit' do
+	# 	content_type 'application/json'
 		
-    $all_convos = $intercom.counts.for_type(type: 'conversation').conversation["open"]
+ #    $all_convos = $intercom.counts.for_type(type: 'conversation').conversation["open"]
 
-		text = "{\"content\":{\"components\":[{\"id\":\"ab1c31592d25779a24e25b2e97b4\",\"type\":\"text\",\"text\":\"There are currently #{$all_convos} conversations in queue.\",\"style\":\"header\",\"align\":\"left\",\"bottom_margin\":false},{\"id\":\"81e44d7877d568cdb57883b1fa20\",\"type\":\"button\",\"label\":\"Refresh \",\"style\":\"primary\",\"action\":{\"type\":\"submit\",\"url\":null},\"bottom_margin\":false}]},\"stored_data\":{}}"
-		text.to_json
-	end
+	# 	text = "{\"content\":{\"components\":[{\"id\":\"ab1c31592d25779a24e25b2e97b4\",\"type\":\"text\",\"text\":\"There are currently #{$all_convos} conversations in queue.\",\"style\":\"header\",\"align\":\"left\",\"bottom_margin\":false},{\"id\":\"81e44d7877d568cdb57883b1fa20\",\"type\":\"button\",\"label\":\"Refresh \",\"style\":\"primary\",\"action\":{\"type\":\"submit\",\"url\":null},\"bottom_margin\":false}]},\"stored_data\":{}}"
+	# 	text.to_json
+	# 	text
+	# end
 end
  
  
