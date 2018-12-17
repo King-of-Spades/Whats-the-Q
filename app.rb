82c3b88856f55a7f21114572b1866a199963eb05
aslonski <andrey@intercom.io>
2018-12-17 08:48:28 -0600

    live canvas testing

--- app.rb
+++ app.rb
@@ -4,6 +4,7 @@ require 'net/http'
 require 'intercom'
 require 'dotenv/load'
 
+
 $intercom = Intercom::Client.new(token: ENV['TOKEN'])
 $all_convos = $intercom.counts.for_type(type: 'conversation').conversation["open"]
 p $all_convos
@@ -23,8 +24,9 @@ post '/live_canvas' do
 		
 $all_convos = $intercom.counts.for_type(type: 'conversation').conversation["open"]
 
-	text = "{\"canvas\":{\"content\":{\"components\":[{\"id\":\"ab1c31592d25779a24e25b2e97b4\",\"type\":\"text\",\"text\":\"There are currently #{$all_convos} conversations in queue.\",\"style\":\"header\",\"align\":\"left\",\"bottom_margin\":false},{\"id\":\"81e44d7877d568cdb57883b1fa20\",\"type\":\"button\",\"label\":\"Refresh \",\"style\":\"primary\",\"action\":{\"type\":\"submit\",\"url\":null},\"bottom_margin\":false}]},\"stored_data\":{}}}"
+	text = "{\"canvas\":{\"content\":{\"components\":[{\"id\":\"ab1c31592d25779a24e25b2e97b4\",\"type\":\"text\",\"text\":\"There are currently #{$all_convos} conversations in queue.\",\"style\":\"header\",\"align\":\"left\",\"bottom_margin\":false},}]},\"stored_data\":{}}}"
  	text.to_json
 	text
 end
+ 
  
