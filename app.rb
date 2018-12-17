7cb8bdcfdddde13ddd6dfa214681f16b308db1eb
aslonski <andrey@intercom.io>
2018-12-17 11:08:31 -0600

    add last updated to canvas

--- app.rb
+++ app.rb
@@ -28,8 +28,8 @@ post '/live_canvas' do
   $all_convos = $intercom.counts.for_type(type: 'conversation').conversation["open"]
 
 
-	text = "{\"content\":{\"components\":[{\"id\":\"ab1c31592d25779a24e25b2e97b4\",\"type\":\"text\",\"text\":\"There are currently *#{$all_convos}* ongoing conversations.
-	Updated at: #{$time}.\",\"style\":\"header\",\"align\":\"left\",\"bottom_margin\":false}]}}"
+	text = "{\"content\":{\"components\":[{\"id\":\"ab1c31592d25779a24e25b2e97b4\",\"type\":\"text\",\"text\":\"Current ongoing conversations: *#{$all_convos}*\\n
+	Updated at: *#{$time}*\",\"style\":\"header\",\"align\":\"left\",\"bottom_margin\":false}]}}"
  	text.to_json
 	text
 
